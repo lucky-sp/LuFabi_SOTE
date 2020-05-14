f4835d60c745fc4a77023fe27652a740c97718bc
luckyspri <lucky.sp@web.de>
2020-05-14 15:23:46 +0200

    fixed car

---
+++ 02_Interfaces_Inheritance/src/Car/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main (final String[] args) {
+        final Car Auto = new Car(65, 180000, "VW", "Polo");
+        System.out.println(Auto.leistung);
+
+        final Car Auto2 = new Car.CarBuilder()
+        .setKilometerstand(12300)
+        .setLeistung(230)
+        .setMarke("Toyota")
+        .setModell("Hilux")
+        .build();
+
+        System.out.println(Auto2.modell);
+    }
+}
