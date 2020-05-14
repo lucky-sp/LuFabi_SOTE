91d7a16947dfc5de1a5619901127880a2574882f
fabrihtwg <fabian.ristau@htwg-konstanz.de>
2020-05-14 15:17:51 +0200

    exercise slide31 building

---
+++ 02_Interfaces_Inheritance/src/Building/Building.java
@@ -0,0 +1,12 @@
+package Building;
+
+public class Building {
+
+    public class Room {
+    }
+
+    Building building = new Building();
+    Room room = building.new Room();
+}
+
+//non-static member class as on instance of Building we create instances of depending rooms
