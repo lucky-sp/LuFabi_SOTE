73fadb4e1f86dae24204dd5c838595268638cbf6
luckyspri <lucky.sp@web.de>
2020-05-26 16:03:10 +0200

    solved enums ex. 1

---
+++ 04_Enums/src/Exercise1/Month.java
@@ -0,0 +1,27 @@
+public enum Month {
+    JANUARY("January", 31, Seasons.WINTER),
+    FEBRUARY("February",28,Seasons.WINTER),
+    MARCH("March", 31, Seasons.WINTER);
+
+    private String myName;
+    private Seasons season;
+    private int amountOfDays;
+
+    private Month(String name, int amountOfDays, Seasons season){
+        this.myName = name;
+        this.amountOfDays = amountOfDays;
+        this.season = season;
+    }
+
+    public String getName() {
+        return this.myName;
+    }
+
+    public int getAmountOfDays(){
+        return this.amountOfDays;
+    }
+
+    public boolean in(Seasons season){
+        return season == this.season;
+    }
+}
