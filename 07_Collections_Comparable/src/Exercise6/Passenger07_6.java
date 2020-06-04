d5037c9d7024f8b69e3cb90e0dac28b63199f480
luckyspri <lucky.sp@web.de>
2020-06-04 13:51:11 +0200

    refactoring

---
+++ 07_Collections_Comparable/src/Exercise6/Passenger07_6.java
@@ -0,0 +1,19 @@
+public class Passenger07_6 {
+    String name;
+    Passanger07_6 nextPassanger;
+
+    public Passenger07_6(String name ){
+        this.name = name;
+    } 
+
+    public Passanger07_6 getNextPassanger() {
+        return nextPassanger;
+    }
+
+    public void setNextPassanger(Passanger07_6 nextPassanger) {
+        this.nextPassanger = nextPassanger;
+    }
+
+    
+
+}
---
+++ 07_Collections_Comparable/src/Exercise6/main07_6.java
@@ -0,0 +1,15 @@
+import sun.jvm.hotspot.tools.SysPropsDumper;
+
+public class main07_6 {
+    
+    public static void main(String[] args){
+        
+        passengerList list = new passengerList();
+
+
+
+        for (Passenger07_6 passenger : colltection){
+            System.out.println(passenger);
+        }
+    }
+}
