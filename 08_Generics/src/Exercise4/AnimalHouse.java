a7df75927f68bef196d67b89dd8ef9d4e5132763
fabrihtwg <fabian.ristau@htwg-konstanz.de>
2020-06-17 14:44:18 +0200

    ex8_4

---
+++ 08_Generics/src/Exercise4/AnimalHouse.java
@@ -0,0 +1,14 @@
+package Exercise4;
+
+public class AnimalHouse<E> {
+    private E animal;
+
+    public void setAnimal(E x) {
+        animal = x;
+    }
+
+    public E getAnimal() {
+        return animal;
+    }
+}
+
---
+++ 08_Generics/src/Exercise4/Cat.java
@@ -0,0 +1,4 @@
+package Exercise4;
+
+public class Cat extends Animal {
+}
